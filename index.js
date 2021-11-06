88663f8abbc3e9cc4d7ac307ae60b9c897af3c7e
MTheuws <89788123+MTheuws@users.noreply.github.com>
2021-11-06 14:08:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Hello Winc Academy")
+let firstName = 'Marijn';
+console.log(firstName);
+let sum = 4 + 4;
+console.log("sum:", sum);
+let sumWithStrings = "4" + "4";
+console.log("sum with strings:", sumWithStrings);
+let subtraction = 7 - 3;
+console.log("subtraction:", subtraction);
+let multiplication = 15 * 6;
+console.log("multiplication:", multiplication);
+let division = 12 / 3;
+console.log("division:", division);
+let modulus = 20 % 10;
+console.log("modulus:", modulus);
+let age = 48;
+console.log(typeof age);
+let ageAsString = "48";
+console.log(typeof ageAsString);
+
+
